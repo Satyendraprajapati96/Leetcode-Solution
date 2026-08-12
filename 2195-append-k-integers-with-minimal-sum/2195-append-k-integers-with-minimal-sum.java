f6861f1ab8d89aaf598805fbe6d23e1f4b77e23f
Satyendra prajapati <Satyendrapraja96@gmail.com>
2026-08-12 23:58:14 +0530

    Time: 26 ms (59.47%), Space: 73 MB (84.09%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.java
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.java
@@ -2,29 +2,20 @@ class Solution {
     public long minimalKSum(int[] nums, int k) {
         Arrays.sort(nums);
          long sum = 0;
-        long current = 1;
-
+        long curr = 1;
         for (int num : nums) {
-
-            if (current > num) {
+            if (curr > num) {
                 continue;
             }
-
-            long count = Math.min(k, num - current);
-
-            sum += (current + current + count - 1) * count / 2;
-
+            long count = Math.min(k, num - curr);
+            sum += (curr + curr + count - 1) * count / 2;
             k -= count;
-            current = num + 1;
-
+            curr = num + 1;
             if (k == 0) {
                 return sum;
             }
         }
-
-        // If we still need numbers, take them after the largest element
-        sum += (current + current + k - 1L) * k / 2;
-
+        sum += (curr + curr + k-1L)*k / 2;
         return sum;
     }
 }
