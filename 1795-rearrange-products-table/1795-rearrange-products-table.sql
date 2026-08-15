a1743de4cf63f81fbb9fb9d270bf4e2947b26e8e
Satyendra prajapati <Satyendrapraja96@gmail.com>
2026-08-15 23:44:49 +0530

    Time: 589 ms (43.89%), Space: 0B (100%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,12 +1,16 @@
 # Write your MySQL query statement below
-SELECT p1.product_id, 'store1' as store, p1.store1 AS price 
-FROM products as p1
-WHERE p1.store1 IS NOT NULL
-UNION
-SELECT p2.product_id, 'store2' as store, p2.store2 AS price 
-FROM products as p2
-WHERE p2.store2 IS NOT NULL
-UNION
-SELECT p3.product_id, 'store3' as store, p3.store3 AS price 
-FROM products as p3
-WHERE p3.store3 IS NOT NULL
+select product_id, 'store1' as store, store1 as price
+from products
+where store1 is not null
+
+union all
+
+select product_id, 'store2' as store, store2 as price
+from products
+where store2 is not null
+
+union all
+
+select product_id, 'store3' as store, store3 as price
+from products
+where store3 is not null;
